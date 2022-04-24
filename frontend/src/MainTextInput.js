dfa3de9ffb591a074f3c4c96a74725e345f03964
Alex Flores Escarcega <afloresescarcega@users.noreply.github.com>
2022-04-24 17:08:21 -0500

    4: provide challenge for users to edit a string in as few keystrokes as possible

--- frontend/src/MainTextInput.js
+++ frontend/src/MainTextInput.js
@@ -1,64 +1,102 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
+
+function getRecordFromCookie() {
+  try {
+    return document.cookie
+      .split(";")
+      .find((row) => row.startsWith("record="))
+      .split("=")[1];
+  } catch (e) {
+    return null;
+  }
+}
 
 export class MainTextInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "The quick brown fox jumps over the lazy dog", keyPressCount: 0 };
+    this.state = {
+      value: "The quick brown jumps fox over the lazy dog",
+      keyPressCount: 0,
+      goal: "The quick brown fox jumps over the lazy dog",
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.mainTextInput = createRef();
   }
 
   componentDidMount() {
-    console.log("finished mounting");
+    console.debug("finished mounting");
+    this.mainTextInput.current.style.backgroundColor = "red";
+    this.mainTextInput.current.addEventListener("click", (e) => {e.stopPropagation();
+      e.preventDefault(); this.mainTextInput.current.focus()}, true);
   }
 
   componentWillUnmount() {
-    console.log("finished unmounting");
+    console.debug("finished unmounting");
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
+    if (event.target.value === this.state.goal) {
+      console.debug("You win!");
+      if (getRecordFromCookie() == null || this.state.keyPressCount < getRecordFromCookie()) {
+        document.cookie = "record=" + this.state.keyPressCount;
+      }
+      this.mainTextInput.current.style.backgroundColor = "green";
+    } else {
+      this.mainTextInput.current.style.backgroundColor = "red";
+      console.debug("Keep going!");
+    }
   }
 
   handleKeyPress(event) {
     this.setState((prevState) => ({
-      keyPressCount: prevState.keyPressCount + 1
+      keyPressCount: prevState.keyPressCount + 1,
     }));
 
     if (event.altKey) {
-      console.log( `You pressed a alt key. ${event.key}` );
+      console.debug(`You pressed a alt key. ${event.key}`);
     } else if (event.shiftKey) {
-      console.log( `You pressed a shift key. ${event.key}` );
+      console.debug(`You pressed a shift key. ${event.key}`);
     } else if (event.ctrlKey) {
-      console.log( `You pressed a control key. ${event.key}` );
+      console.debug(`You pressed a control key. ${event.key}`);
     } else if (event.metaKey) {
-      console.log(`You pressed a meta key. ${event.key}`);
+      console.debug(`You pressed a meta key. ${event.key}`);
     } else {
-      console.log( `You pressed a key. ${String.fromCharCode(event.charCode)}` );
+      console.debug(`You pressed a key. ${String.fromCharCode(event.charCode)}`);
     }
-    console.log(`Pressed total of ${this.state.keyPressCount} times.`);
   }
 
   render() {
+    let previousRecord;
+    if (getRecordFromCookie()) {
+      previousRecord = (
+        <p>Previous record: {getRecordFromCookie()} keystrokes</p>
+      );
+    }
     return (
-        <div id="textboxes">
-          <textarea id="counter"
-                    rows="1"
-                    cols="3"
-                    value={this.state.keyPressCount}
-                    onChange={this.handleChange}
-          />
-          <textarea
-            autoFocus="autofocus"
-            id="mainTextInput"
-            rows="1"
-            cols="50"
-            value={this.state.value}
-            onChange={this.handleChange}
-            onKeyDown={(e) => this.handleKeyPress(e)}
-          />
-        </div>
+      <div id="textboxes">
+        {previousRecord}
+        <p>Goal: "{this.state.goal}"</p>
+        <textarea
+          id="counter"
+          rows="1"
+          cols="3"
+          value={this.state.keyPressCount}
+          onChange={this.handleChange}
+        />
+        <textarea
+          ref={this.mainTextInput}
+          autoFocus="autofocus"
+          id="mainTextInput"
+          rows="1"
+          cols="50"
+          value={this.state.value}
+          onChange={this.handleChange}
+          onKeyDown={(e) => this.handleKeyPress(e)}
+        />
+      </div>
     );
   }
 }
