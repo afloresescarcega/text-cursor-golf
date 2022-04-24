9d60d7f251efa1f5531ca193a1217ae22b1ea7ec
Alex Flores Escarcega <afloresescarcega@users.noreply.github.com>
2022-04-24 14:17:07 -0500

    3: Be able to track keys pressed and update counter

--- frontend/src/MainTextInput.js
+++ frontend/src/MainTextInput.js
@@ -3,9 +3,10 @@ import { Component } from "react";
 export class MainTextInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "The quick brown fox jumps over the lazy dog" };
+    this.state = { value: "The quick brown fox jumps over the lazy dog", keyPressCount: 0 };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   componentDidMount() {
@@ -20,15 +21,44 @@ export class MainTextInput extends Component {
     this.setState({ value: event.target.value });
   }
 
+  handleKeyPress(event) {
+    this.setState((prevState) => ({
+      keyPressCount: prevState.keyPressCount + 1
+    }));
+
+    if (event.altKey) {
+      console.log( `You pressed a alt key. ${event.key}` );
+    } else if (event.shiftKey) {
+      console.log( `You pressed a shift key. ${event.key}` );
+    } else if (event.ctrlKey) {
+      console.log( `You pressed a control key. ${event.key}` );
+    } else if (event.metaKey) {
+      console.log(`You pressed a meta key. ${event.key}`);
+    } else {
+      console.log( `You pressed a key. ${String.fromCharCode(event.charCode)}` );
+    }
+    console.log(`Pressed total of ${this.state.keyPressCount} times.`);
+  }
+
   render() {
     return (
-      <textarea
-        id="mainTextInput"
-        rows="1"
-        cols="50"
-        value={this.state.value}
-        onChange={this.handleChange}
-      />
+        <div id="textboxes">
+          <textarea id="counter"
+                    rows="1"
+                    cols="3"
+                    value={this.state.keyPressCount}
+                    onChange={this.handleChange}
+          />
+          <textarea
+            autoFocus="autofocus"
+            id="mainTextInput"
+            rows="1"
+            cols="50"
+            value={this.state.value}
+            onChange={this.handleChange}
+            onKeyDown={(e) => this.handleKeyPress(e)}
+          />
+        </div>
     );
   }
 }
